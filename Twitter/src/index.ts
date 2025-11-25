7d1260ef2487d5644a3eab315848667da5f458e6
Binard2112 <mait58674@gmail.com>
2025-11-25 15:50:52 +0700

    connect to mongo atlas by mongodb driver and create directory tree structure

--- Twitter/src/index.ts
+++ Twitter/src/index.ts
@@ -1,4 +1,6 @@
 import express = require('express')
+import usersRouter from './routes/users.routes'
+import databaseService from './services/database.services'
 
 const app = express()
 const PORT = 3000
@@ -6,6 +8,11 @@ const PORT = 3000
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+//middleware
+app.use(express.json())
+app.use('/users', usersRouter)
+
+databaseService.connect()
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
---
+++ Twitter/src/services/database.services.ts
@@ -0,0 +1,30 @@
+import { config } from 'dotenv'
+import { MongoClient } from 'mongodb'
+
+config()
+console.log(process.env.USERNAME)
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.DB_PASSWORD}@cluster0.dvz1yko.mongodb.net/?appName=Cluster0`
+
+class DatabaseService {
+  private client: MongoClient
+  constructor() {
+    this.client = new MongoClient(uri)
+  }
+
+  async connect() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await this.client.connect()
+      // Send a ping to confirm a successful connection
+      await this.client.db('admin').command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await this.client.close()
+    }
+  }
+}
+
+const databaseService = new DatabaseService()
+
+export default databaseService
