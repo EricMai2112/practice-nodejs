bc68ce29c3a1d29ad29882e50e6b5f8aa1acd185
EricMai2112 <mait58674@gmail.com>
2025-11-30 13:43:57 +0700

    handle bug when users request many unwanted items like tokens

--- Twitter/src/routes/users.routes.ts
+++ Twitter/src/routes/users.routes.ts
@@ -11,6 +11,7 @@ import {
   updateMeController,
   verifyFotgotPasswordController
 } from '~/controllers/users.controllers'
+import { filterMiddleware } from '~/middlewares/common.middlewares'
 import {
   accessTokenValidator,
   emailVerifyTokenValidator,
@@ -23,6 +24,7 @@ import {
   verifiedUserValidator,
   verifyForgotPasswordTokenValidator
 } from '~/middlewares/users.middlewares'
+import { UpdateMeReqBody } from '~/models/requests/User.requests'
 import { wrapRequestHandler } from '~/utils/handlers'
 
 const usersRouter = Router()
@@ -92,6 +94,16 @@ usersRouter.patch(
   accessTokenValidator,
   verifiedUserValidator,
   updateMeValidator,
+  filterMiddleware<UpdateMeReqBody>([
+    'name',
+    'date_of_birth',
+    'bio',
+    'website',
+    'location',
+    'username',
+    'avatar',
+    'cover_photo'
+  ]),
   wrapRequestHandler(updateMeController)
 )
 export default usersRouter
