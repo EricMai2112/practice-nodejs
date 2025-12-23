0bfeeac95e42abb542d3caa868c85712e95d1b4e
EricMai2112 <mait58674@gmail.com>
2025-12-23 13:13:03 +0700

    handle create hashtags and save into database Hashtag

--- Twitter/src/services/tweets.services.ts
+++ Twitter/src/services/tweets.services.ts
@@ -1,15 +1,29 @@
 import { TweetReqBody } from '~/models/requests/Tweet.requests'
 import databaseService from './database.services'
 import Tweet from '~/models/schemas/Tweet.schema'
-import { ObjectId } from 'mongodb'
+import { ObjectId, WithId } from 'mongodb'
+import Hashtag from '~/models/schemas/Hashtag.schema'
 
 class TweetService {
+  async checkAndCreateHashtag(hashtags: string[]) {
+    const hashtagDocuments = await Promise.all(
+      hashtags.map((hashtag) => {
+        return databaseService.hashtags.findOneAndUpdate(
+          { name: hashtag },
+          { $setOnInsert: new Hashtag({ name: hashtag }) },
+          { upsert: true, returnDocument: 'after' }
+        )
+      })
+    )
+    return hashtagDocuments.map((hashtag) => (hashtag as WithId<Hashtag>)._id)
+  }
   async createTweet(body: TweetReqBody, user_id: string) {
+    const hashtags = await this.checkAndCreateHashtag(body.hashtags)
     const result = await databaseService.tweets.insertOne(
       new Tweet({
         audience: body.audience,
         content: body.content,
-        hashtags: [],
+        hashtags,
         mentions: body.mentions,
         medias: body.medias,
         parent_id: body.parent_id,
