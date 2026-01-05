5169d150208d64a9f721fae3add8b0c4e4049d84
EricMai2112 <mait58674@gmail.com>
2026-01-05 22:54:41 +0700

    handle private message between two people

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,21 +1,50 @@
-import React, { useEffect } from "react";
-import { io } from "socket.io-client";
+import React, { useEffect, useState } from "react";
+import socket from "./socket";
 
 export default function Chat() {
+  const profile = JSON.parse(localStorage.getItem("profile")) || {};
+  const [value, setValue] = useState("");
+  const [messages, setMessages] = useState([]);
   useEffect(() => {
-    const socket = io(import.meta.env.VITE_API_URL);
-    socket.on("connect", () => {
-      console.log(socket.id);
-      socket.emit("hello", "Hello I'm Eric Mai");
-      socket.on("hi", (arg) => console.log(arg))
-    });
-    socket.on("disconnect", () => {
-      console.log(socket.id);
+    socket.auth = {
+      _id: profile._id,
+    };
+    socket.connect();
+    socket.on("receive private message", (data) => {
+      const content = data.content;
+      setMessages((message) => [...message, content]);
     });
     //cleanup function to disconnect
     return () => {
       socket.disconnect();
     };
-  }, []);
-  return <div>Chat</div>;
+  }, [profile._id]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValue("");
+    socket.emit("private message", {
+      content: value,
+      to: "6954afe18c44e5499b57faa5", //user_id
+    });
+  };
+  return (
+    <div>
+      <h1>Chat</h1>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>
+            <div>{message}</div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={value}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 }
